aff0d663e9b773b6dccf60a286a4ee0643e22efa
Habashi94 <56684714+Habashi94@users.noreply.github.com>
2022-09-28 16:02:00 +0100

    Updated passing of first test

---
+++ src/basket.js
@@ -0,0 +1,16 @@
+class Basket {
+
+    basketItems = [];
+
+
+    add = item => {
+        this.basketItems.push(item);
+    };
+
+    remove = item => {
+        this.basketItems.pop(item)
+
+    }
+};
+
+module.exports = Basket;
--- test/basket.spec.js
+++ test/basket.spec.js
@@ -35,3 +35,32 @@ item = null;
 expected = undefined;
 actual = undefined;
 result = undefined;
+
+
+
+// console.log(`==================`)
+// console.log(`Test 2 - removes something from the basket`)
+
+// // Arrange
+// basket = new Basket();
+// expected = 1;
+
+// // Act
+// basket.add({ id: 'item1' })
+// basket.add({ id: 'item2' })
+// basket.remove()
+// actual = basket.basketItems.length
+
+
+// // Assert
+// result = assertEquals(actual, expected)
+// console.log(`Test 2: Something removed from basket basket: ${result}`)
+
+// // Cleanup
+
+// basket = null;
+// item = null;
+// expected = undefined;
+// actual = undefined;
+// result = undefined;
+
